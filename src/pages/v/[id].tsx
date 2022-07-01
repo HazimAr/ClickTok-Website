6c17f1bcb09fcc990275bfde5b9ea8ec89197237
Hazim Arafa <hazimarafa80@gmail.com>
2022-07-01 03:13:56 -0700

    more info for video

--- src/pages/v/[id].tsx
+++ src/pages/v/[id].tsx
@@ -1,29 +1,78 @@
-import { Button, Heading, HStack, Link, Text, VStack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Divider,
+  Heading,
+  HStack,
+  Icon,
+  Link,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Container from "@components/Container";
 import ContainerInside from "@components/ContainerInside";
 import EnhancedChakraLink from "@components/EnhancedChakraLink";
 import { NextPageContext } from "next";
 import Head from "next/head";
+import { FaMusic } from "react-icons/fa";
 
 export default function ({ video }) {
   return (
     <>
       <Head>
-        <title>{video.author || "Error Loading Video"} | ClickTok</title>
+        <title>
+          {video
+            ? `Check out ${video.author.name}'s video!`
+            : "Error Loading Video"}{" "}
+          | ClickTok
+        </title>
+        <meta
+          property="og:title"
+          content={`${
+            video
+              ? `Check out ${video.author.name}'s video!`
+              : "Error Loading Video"
+          } | ClickTok`}
+        />
         <meta
           property="og:description"
           content={
-            video.author
-              ? `Check out ${video.author}'s video!`
+            video
+              ? video.description
               : "This can be due to TikTok taking down the video or the video being deleted."
           }
         />
       </Head>
       <Container py={10}>
-        <ContainerInside as={VStack}>
-          {video.url ? (
+        <ContainerInside as={Stack} w="400px">
+          {video ? (
             <>
-              <video src={video.url} width="300" controls />
+              <VStack>
+                <video
+                  src={video.url}
+                  width="75%"
+                  controls
+                  style={{ borderRadius: "10px" }}
+                />
+              </VStack>
+              <Text>{video.description}</Text>
+              <HStack>
+                <Icon as={FaMusic} />
+                <Text>{video.music}</Text>
+              </HStack>
+              <Divider />
+              <HStack py={2}>
+                <Avatar src={video.author.icon} name={video.author.name} />
+                <Stack spacing={0}>
+                  <Heading fontSize="lg">{video.author.username}</Heading>
+                  <Text>
+                    {video.author.name} •{" "}
+                    {new Date(video.created * 1000).toLocaleDateString()}
+                  </Text>
+                </Stack>
+              </HStack>
+              <Divider />
               <HStack>
                 <Button
                   as={Link}
@@ -85,12 +134,23 @@ export async function getServerSideProps({ query }: NextPageContext) {
     await fetch(`https://api2.musical.ly/aweme/v1/aweme/detail/?aweme_id=${id}`)
   ).json();
 
+  if (!video?.aweme_detail)
+    return {
+      props: { video: null },
+    };
   return {
     props: {
       video: {
-        url: video?.aweme_detail?.video?.download_addr?.url_list?.[0] || null,
-        author: video?.aweme_detail?.author?.unique_id || null,
-        id: video?.aweme_detail?.aweme_id || null,
+        url: video.aweme_detail.video.download_addr.url_list[0],
+        description: video.aweme_detail.desc,
+        music: video.aweme_detail.music.title,
+        author: {
+          name: video.aweme_detail.author.nickname,
+          username: video.aweme_detail.author.unique_id,
+          icon: video.aweme_detail.author.avatar_thumb.url_list[0],
+        },
+        id: video.aweme_detail.aweme_id,
+        created: video.aweme_detail.create_time,
       },
     },
   };
