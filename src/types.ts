0542f4c2074f3f2972ae87a1b4831c5e3c5df57d
Hazim Arafa <hazimarafa80@gmail.com>
2022-07-05 12:41:08 -0700

    [id] leaderboards

--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,12 @@ export type LeaderboardGuild = {
   name: string;
   icon: string;
   conversions: number;
-  createdAt: string;
+  createdAt: Date;
+};
+
+export type LeaderboardGuildUser = {
+  name: string;
+  avatarURL: string;
+  conversions: number;
+  createdAt: Date;
 };
