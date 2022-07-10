091f3089b4e50f6595a2d4f27cd9bc5795434033
Hazim <hazimarafa80@gmail.com>
2022-07-10 16:27:00 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 const GA_TRACKING_ID = "G-PGZQKZHHW0";
 
 const META = {
-  title: "ClickTok | A Simple Tiktok Discord Embed Bot",
+  title: "ClickTok | All in one TikTok Discord Bot",
   description:
     "ClickTok is a simple Discord bot that allows you to embed Tiktok videos in Discord channels.",
   url: "https://clicktok.xyz",
