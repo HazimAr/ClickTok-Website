63df767684856d1847ead437bebbfebe89e2570e
ChickPS5 <108319483+ChickParam@users.noreply.github.com>
2022-07-16 13:42:36 -0400

    Update About.tsx

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -7,7 +7,7 @@ const data = [
   {
     title: "Easily embed Tiktok videos",
     description:
-      "ClickTok is a simple Discord bot that allows you to embed Tiktok videos easily in your Discord server.",
+      "ClickTok is a simple Discord bot that allows you to embed Tiktok videos easily in your Discord server. The official TikTok verified embedding bot! ",
     button: "Start Embedding",
     img: "/embed.gif",
   },
