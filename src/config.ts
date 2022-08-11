360dd1ed8a6839418a399caf10e8d8bfb885b707
Hazim Arafa <hazimarafa80@gmail.com>
2022-08-10 18:10:40 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,6 @@ const META = {
   image: "/logo.png",
 };
 
-export const API = "https://3bce-70-173-133-195.ngrok.io";
+export const API = "https://b11a-70-173-133-195.ngrok.io";
 
 export { GA_TRACKING_ID, META };
